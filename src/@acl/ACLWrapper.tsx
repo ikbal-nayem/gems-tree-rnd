6b5a90248e0b104ad5d175d236842eaec0ddedd2
Ikbal <ikbalnayem000@gmail.com>
2023-06-22 10:33:51 +0600

    acl update

--- src/@acl/ACLWrapper.tsx
+++ src/@acl/ACLWrapper.tsx
@@ -4,6 +4,7 @@ import { FC, ReactNode } from "react";
 type ACLWrapperProps = {
 	visibleCustom?: boolean; // pass a custom boolean value by conditional check
 	visibleToRoles?: string[]; // pass role list to show ['APPROVER', 'REVIEWER']
+	invisibleToRoles?: string[]; // pass role list to show ['APPROVER', 'REVIEWER']
 	children?: ReactNode;
 	showInstead?: ReactNode | string; // Show the block or text instead of the orginal content
 };
@@ -12,11 +13,18 @@ type ACLWrapperProps = {
 const ACLWrapper: FC<ACLWrapperProps> = ({
 	visibleCustom = true,
 	visibleToRoles,
+	invisibleToRoles,
 	children,
 	showInstead,
 }) => {
 	const { currentUser } = useAuth();
 	let hasPermission = visibleCustom;
+	if (invisibleToRoles?.length) {
+		const hasRolePermission = currentUser?.roles?.some((ro) =>
+			invisibleToRoles?.some((vt) => vt === ro.roleCode)
+		);
+		hasPermission = !hasRolePermission && visibleCustom;
+	}
 	if (visibleToRoles?.length) {
 		const hasRolePermission = currentUser?.roles?.some((ro) =>
 			visibleToRoles?.some((vt) => vt === ro.roleCode)
