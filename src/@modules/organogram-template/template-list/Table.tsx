efe06aaea456593ba59f5cfc029c7e45ea10c93c
Md. Wasifur Rahman <wasif.gems@mopa.gov.bd>
2023-10-19 15:17:34 +0600

    Template List : UI fixing

--- src/@modules/organogram-template/template-list/Table.tsx
+++ src/@modules/organogram-template/template-list/Table.tsx
@@ -1,96 +1,85 @@
 import {
-	ContentPreloader,
-	ITableHeadColumn,
-	IconButton,
-	NoData,
-	Table,
-	TableCell,
-	TableRow,
-	useEmployeeProfile,
+  ContentPreloader,
+  Dropdown,
+  DropdownItem,
+  ITableHeadColumn,
+  Icon,
+  NoData,
+  Table,
+  TableCell,
+  TableRow,
 } from "@gems/components";
-import {
-	COMMON_LABELS,
-	DATE_PATTERN,
-	IMeta,
-	IObject,
-	generateDateFormat,
-	generateRowNumBn,
-	numEnToBn,
-	prepareDuration,
-} from "@gems/utils";
+import { COMMON_LABELS, IMeta, generateRowNumBn } from "@gems/utils";
 import { FC, ReactNode } from "react";
 import { LABELS } from "./labels";
 
 type TableProps = {
-	children: ReactNode;
-	dataList: any[];
-	isLoading: boolean;
-	respMeta?: IMeta;
-	filterProps?: IObject;
+  children: ReactNode;
+  dataList: any[];
+  isLoading: boolean;
+  respMeta?: IMeta;
 };
 
 const TemplateTable: FC<TableProps> = ({
-	children,
-	dataList,
-	isLoading,
-	respMeta,
-	filterProps,
+  children,
+  dataList,
+  isLoading,
+  respMeta,
 }) => {
+  const columns: ITableHeadColumn[] = [
+    { title: COMMON_LABELS.SL_NO, width: 50},
+    { title: LABELS.NAME, width: 250 },
+    { title: COMMON_LABELS.ACTION, width: 80, align: "end" },
+  ];
 
-	const columns: ITableHeadColumn[] = [
-		{ title: COMMON_LABELS.SL_NO, width: 50, align: "center" },
-		{ title: LABELS.NAME, width: 250 },
-		{ title: COMMON_LABELS.ACTION, width: 80, align: "end" },
-	];
-
-	return (
-		<>
-			{dataList?.length ? (
-				<Table columns={columns}>
-					{dataList?.map((item, idx) => (
-						<TableRow key={idx}>
-							<TableCell
-								text={generateRowNumBn(idx, respMeta)}
-								textAlign="center"
-								verticalAlign="top"
-							/>
-							<TableCell
-								text={numEnToBn(item?.govtId) || "-"}
-								textAlign="center"
-								verticalAlign="top"
-							/>
-							<TableCell verticalAlign="top">
-
-							</TableCell>
-							<TableCell verticalAlign="top">
-
-							</TableCell>
-							<TableCell>{prepareDuration(item?.totalDuration)}</TableCell>
-							<TableCell textAlign="end" verticalAlign="top">
-								<IconButton
-									iconName="edit"
-									iconColor="primary"
-									onClick={() => null}
-								/>
-
-								<IconButton
-									iconName="delete"
-									iconColor="danger"
-									onClick={() => null}
-								/>
-
-							</TableCell>
-						</TableRow>
-					))}
-				</Table>
-			) : isLoading ? (
-				<ContentPreloader />
-			) : (
-				<NoData details="কোনো টেমপ্লেটের তথ্য পাওয়া যায়নি!" />
-			)}
-			{children}
-		</>
-	);
+  return (
+    <>
+      {dataList?.length ? (
+        <Table columns={columns}>
+          {dataList?.map((item, idx) => (
+            <TableRow key={idx}>
+              <TableCell
+                text={generateRowNumBn(idx, respMeta)}
+                verticalAlign="top"
+              />
+              <TableCell
+                text={item?.version || COMMON_LABELS.NOT_ASSIGN}
+                verticalAlign="top"
+              />
+              <TableCell textAlign="end" verticalAlign="top">
+                <Dropdown
+                  btnIcon={true}
+                  btnContent={<Icon icon="more_vert" size={20} />}
+                  id={item?.id}
+                >
+                  <DropdownItem onClick={() => null}>
+                    <Icon size={19} icon="edit" />
+                    <h6 className="mb-0 ms-3">সম্পাদনা করুন</h6>
+                  </DropdownItem>
+                  <DropdownItem onClick={() => null}>
+                    <Icon size={19} icon="delete" color="danger" />
+                    <h6 className="mb-0 ms-3 text-danger">মুছে ফেলুন</h6>
+                  </DropdownItem>
+                </Dropdown>
+              </TableCell>
+            </TableRow>
+          ))}
+        </Table>
+      ) : isLoading ? (
+        <ContentPreloader />
+      ) : (
+        // <Table columns={columns}>
+        //   <TableRow>
+        //     <TableCell />
+        //     <TableCell>
+        <NoData details="কোনো টেমপ্লেটের তথ্য পাওয়া যায়নি!" />
+        //     </TableCell>
+        //   </TableRow>
+        // </Table>
+      )}
+      {children}
+    </>
+  );
 };
 
 export default TemplateTable;
--- src/@modules/organogram-template/template-list/index.tsx
+++ src/@modules/organogram-template/template-list/index.tsx
@@ -1,16 +1,20 @@
 import { PageTitle } from "@context/PageData";
 import {
-	DownloadMenu,
-	Pagination,
-	Separator,
+  DownloadMenu,
+  Input,
+  ListDownload,
+  Pagination,
+  Separator,
 } from "@gems/components";
 import {
-	DATE_PATTERN,
-	IMeta,
-	IObject,
-	generateDateFormat,
-	numEnToBn,
-	searchParamsToObject,
+  COMMON_LABELS,
+  DATE_PATTERN,
+  IMeta,
+  IObject,
+  generateDateFormat,
+  numEnToBn,
+  searchParamsToObject,
+  useDebounce,
 } from "@gems/utils";
 import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
@@ -19,105 +23,142 @@ import { MENU } from "@constants/menu-titles.constant";
 import TemplateTable from "./Table";
 
 const initPayload = {
-	meta: {
-		page: 0,
-		limit: 10,
-		sort: [{ order: "asc", field: "govtId" }],
-	},
-	body: { searchKey: "" },
+  meta: {
+    page: 0,
+    limit: 10,
+    sort: [
+      {
+        order: "desc",
+        field: "createdOn",
+      },
+    ],
+  },
+  body: { searchKey: "" },
 };
 
 const TemplateList = () => {
-	const [dataList, setDataList] = useState<IObject[]>();
-	const [respMeta, setRespMeta] = useState<IMeta>();
-	const [isLoading, setLoading] = useState<boolean>(false);
-	const [searchParams] = useSearchParams();
-	const reqPayload = useRef(initPayload);
-	const isInit = useRef<boolean>(true);
-	const filterProps = useRef<IObject>({});
+  const [dataList, setDataList] = useState<IObject[]>();
+  const [respMeta, setRespMeta] = useState<IMeta>();
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-	useEffect(() => {
-		const params = searchParamsToObject(searchParams);
-		if (params?.page || params?.limit)
-			reqPayload.current.meta = {
-				...reqPayload.current.meta,
-				page: +params?.page || 0,
-				limit: +params?.limit || 10,
-			};
-		// if (!isInit.current) getEmployeeList();
-		else isInit.current = false;
-	}, [searchParams]);
+  const reqPayload = useRef(initPayload);
+  const filterProps = useRef<IObject>({});
+  const params: any = searchParamsToObject(searchParams);
+  const [search, setSearch] = useState<string>(
+    searchParams.get("searchKey") || ""
+  );
+  const searchKey = useDebounce(search, 500);
 
-	const onFilter = (filterBody) => {
-		filterProps.current = filterBody;
-		reqPayload.current.meta.page = 0;
-		reqPayload.current.body = { ...reqPayload.current.body, ...filterBody };
-		// getEmployeeList();
-	};
+  useEffect(() => {
+    if (searchKey) params.searchKey = searchKey;
+    else delete params.searchKey;
+    setSearchParams({ ...params });
+    // eslint-disable-next-line
+  }, [searchKey, setSearchParams]);
 
-	// const getEmployeeList = () => {
-	// 	topProgress.show();
-	// 	setLoading(true);
-	// 	ReportService.getDCPromotableEmployeeList(reqPayload.current)
-	// 		.then((resp) => {
-	// 			setEmployeeList(resp?.body);
-	// 			setRespMeta(resp?.meta);
-	// 		})
-	// 		.catch((err) => {
-	// 			toast.error(err?.message);
-	// 			setEmployeeList([]);
-	// 			setRespMeta({});
-	// 		})
-	// 		.finally(() => {
-	// 			topProgress.hide();
-	// 			setLoading(false);
-	// 		});
-	// };
+  useEffect(() => {
+    getDataList();
+  }, [searchParams]);
 
-	return (
-		<>
-			<PageTitle> {MENU.BN.TEMPLATE_LIST} </PageTitle>
-			<div className="card p-4">
-				{/* <Filter onFilter={onFilter} /> */}
-				{/* <Separator /> */}
-				{!!dataList?.length && (
-					<div className="d-flex justify-content-between gap-3">
-						<div />
-						<div className="text-primary text-center">
-							{!!filterProps?.current?.batchToDto?.length && (
-								<>
-									<br />
-									<span>
-										ব্যাচ:&nbsp;
-										{filterProps?.current?.batchToDto
-											?.sort((a, b) => +a?.code - +b?.code)
-											?.map((b) => b?.titleBn)
-											.join(", ")}
-									</span>
-								</>
-							)}
-							<h5 className="my-3">
-								মোট {numEnToBn(respMeta?.totalRecords)} জন
-							</h5>
-						</div>
-						<DownloadMenu
-							fnDownloadPDF={() =>
-								downloadAsPDF(reqPayload.current, respMeta?.totalRecords)
-							}
-						/>
-					</div>
-				)}
-				<TemplateTable
-					dataList={dataList}
-					respMeta={respMeta}
-					isLoading={isLoading}
-					filterProps={filterProps.current}
-				>
-					<Pagination meta={respMeta} pageNeighbours={2} setSearchParams />
-				</TemplateTable>
-			</div>
-		</>
-	);
+  const getDataList = () => {
+    // 	topProgress.show();
+    // 	setLoading(true);
+    // 	ReportService.getDCPromotableEmployeeList(reqPayload.current)
+    // 		.then((resp) => {
+    // 			setEmployeeList(resp?.body);
+    // 			setRespMeta(resp?.meta);
+    // 		})
+    // 		.catch((err) => {
+    // 			toast.error(err?.message);
+    // 			setEmployeeList([]);
+    // 			setRespMeta({});
+    // 		})
+    // 		.finally(() => {
+    // 			topProgress.hide();
+    // 			setLoading(false);
+    // 		});
+  };
+
+  const getXLSXStoreList = (reqMeta = null) => {
+    const payload = {
+      meta: reqMeta ? { ...reqMeta } : { ...respMeta, page: 0, sort: null },
+      body: {
+        searchKey: searchKey || null,
+      },
+    };
+
+    // OMSService.getInventoryList(payload)
+    //   .then((res) => {
+    //     exportXLSX(exportData(res?.body || []), "Template list");
+    //   })
+    //   .catch((err) => toast.error(err?.message))
+    //   .finally(() => {
+    //     setIsLoading(false);
+    //   });
+  };
+
+  const exportData = (data: any[]) =>
+    data.map((d) => ({
+      "টেমপ্লেটের নাম": d?.inventoryTypeBn || COMMON_LABELS.NOT_ASSIGN,
+    }));
+
+  return (
+    <>
+      <PageTitle> {MENU.BN.TEMPLATE_LIST} </PageTitle>
+      <div className="card p-4">
+        {/* <Filter onFilter={onFilter} /> */}
+        {/* <Separator /> */}
+        <div className="d-flex gap-3 mb-4">
+          <Input
+            type="search"
+            noMargin
+            placeholder="টেমপ্লেটের নাম অনুসন্ধান করুন ..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <ListDownload
+            fnDownloadExcel={() =>
+              getXLSXStoreList({
+                page: 0,
+                limit: respMeta?.totalRecords || 0,
+              })
+            }
+            // fnDownloadPDF={() =>
+            //   downloadAsPDF(reqPayload.current, respMeta?.totalRecords)
+            // }
+          />
+        </div>
+        {!!dataList?.length && (
+          <div className="d-flex justify-content-between gap-3">
+            <div className="text-primary text-center">
+              <h5 className="my-3">
+                মোট টেমপ্লেট {numEnToBn(respMeta?.totalRecords)} টি
+              </h5>
+            </div>
+          </div>
+        )}
+
+        {/* ============================================================ TABLE STARTS ============================================================ */}
+
+        <div className="p-4">
+          <TemplateTable
+              dataList={dataList}
+            // dataList={[
+            //   { version: "name A" },
+            //   { version: "name B" },
+            // ]}
+            respMeta={respMeta}
+            isLoading={isLoading}
+          >
+            <Pagination meta={respMeta} pageNeighbours={2} setSearchParams />
+          </TemplateTable>
+        </div>
+
+        {/* ============================================================ TABLE ENDS ============================================================ */}
+      </div>
+    </>
+  );
 };
 
 export default TemplateList;
