f892ec6085d608585f8bc6e2cf8a4c63305a891f
Md. Wasifur Rahman <wasif.gems@mopa.gov.bd>
2023-11-29 12:15:00 +0600

    minor changes

--- src/@modules/organogram-template/components/templateViewComponent/components/EquipmentsList.tsx
+++ src/@modules/organogram-template/components/templateViewComponent/components/EquipmentsList.tsx
@@ -1,6 +1,7 @@
 import { LABELS } from "@constants/common.constant";
 import { Separator } from "@gems/components";
 import { IObject, numEnToBn } from "@gems/utils";
+import { isNotEmptyList } from "utility/utils";
 
 interface IEquipmentsForm {
   data: IObject[];
@@ -17,13 +18,14 @@ const EquipmentsForm = ({ data, inventoryData, langEn }: IEquipmentsForm) => {
       </div>
       <Separator className="mt-1 mb-2" />
       <div className="row">
-        {inventoryData?.length>0 &&
+        {isNotEmptyList(inventoryData) &&
           inventoryData?.map((item, i) => {
             return (
               <div className="col-md-6 col-12" key={i}>
-                <p className="fs-5 fw-bold mb-0">
+                <span className="fs-5 fw-bold">{i + 1 + ". "}</span>
+                <u className="fs-5 fw-bold mb-0">
                   {langEn ? item?.inventoryTypeEn : item?.inventoryTypeBn}
-                </p>
+                </u>
                 <ol type="a">
                   {item?.itemList.map((d, idx) => {
                     return (
@@ -38,19 +40,16 @@ const EquipmentsForm = ({ data, inventoryData, langEn }: IEquipmentsForm) => {
             );
           })}
       </div>
-      <div className="card-head d-flex justify-content-between align-items-center">
-        <h4 className="m-0">{LABEL.MISCELLANEOUS}</h4>
+      <div className="card-head d-flex justify-content-start align-items-center gap-2">
+        <span className="fs-5 fw-bold">3.</span>
+        <u className="fs-5 fw-bold m-0">{LABEL.MISCELLANEOUS}</u>
       </div>
       <Separator className="mt-1 mb-2" />
       <div>
-        <ol className={langEn ? "" : "bn_ol"}>
+        <ol type="a" className={langEn ? "" : "bn_ol"}>
           {data?.length > 0 &&
             data?.map((item, i) => {
-              return (
-                <li key={i}>
-                  &nbsp;&nbsp;{langEn ? item?.titleEn : item?.titleBn}
-                </li>
-              );
+              return <li key={i}>{langEn ? item?.titleEn : item?.titleBn}</li>;
             })}
         </ol>
       </div>
