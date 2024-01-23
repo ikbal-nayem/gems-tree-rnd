3b2a39e476f2dc5f801ff26b6bbb7e4bafed6e39
Md. Wasifur Rahman <wasif.gems@mopa.gov.bd>
2024-01-23 13:37:46 +0600

    Organogram Lists: Redirect Fixation

--- src/@modules/organogram/approved-list/Table.tsx
+++ src/@modules/organogram/approved-list/Table.tsx
@@ -97,7 +97,7 @@ const OrganogramTable: FC<TableProps> = ({
                     <h6 className="mb-0 ms-3">দেখুন</h6>
                   </DropdownItem>
                   <DropdownItem onClick={() => onClone(item)}>
-                    <Icon size={19} icon="file_copy" />
+                    <Icon size={19} icon="edit" />
                     <h6 className="mb-0 ms-3">হালনাগাদ করুন</h6>
                   </DropdownItem>
                   {/* <DropdownItem onClick={() => null}>
