61863b2bb281a7537ab8c93d96197bc636a68df2
pronab.swe <pronab.swe@gmail.com>
2024-03-03 12:26:27 +0600

    orgaization type tree structure ok

--- src/@modules/organization/organizationTypeList/tree/index.tsx
+++ src/@modules/organization/organizationTypeList/tree/index.tsx
@@ -1,65 +1,40 @@
-import {
-  Autocomplete,
-  ContentPreloader,
-  NoData,
-  toast,
-} from "@gems/components";
-import { IObject } from "@gems/utils";
-import { OMSService } from "@services/api/OMS.service";
-import { useEffect, useState } from "react";
-import OrganizationTemplateTree from "./OrganizationTreeComponent/index";
 import { MENU } from "@constants/menu-titles.constant";
 import { PageTitle } from "@context/PageData";
+import { ContentPreloader, NoData } from "@gems/components";
+import { IObject } from "@gems/utils";
+import { useEffect, useState } from "react";
+import OrganizationTemplateTree from "./OrganizationTreeComponent/index";
 
 const Tree = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<IObject>({});
-  const [organiztionaData, setOrganizationData] = useState<IObject>(null);
-  const [organizationList, setOrganizationList] = useState<IObject[]>([]);
 
   useEffect(() => {
-    OMSService.FETCH.ministryDivisionDepartmentList()
-      .then((resp) => setOrganizationList(resp?.body || []))
-      .catch((err) => toast.error(err?.message));
+    getTreeData();
   }, []);
 
-  const onOrganizationChange = (e) => {
-    setOrganizationData(e);
-    if (e?.id) {
-      setLoading(true);
-      OMSService.FETCH.oranizationTreeByOrganizationId(e?.id)
-        .then((res) => {
-          setData(res?.body || {});
-        })
-        .catch((err) => {
-          toast.error(err?.message);
-          setData({});
-        })
-        .finally(() => setLoading(false));
-    } else setData({});
+  const getTreeData = () => {
+    // setLoading(true);
+    // OMSService.FETCH.oranizationTreeByOrganizationId(e?.id)
+    //   .then((res) => {
+    //     setData(res?.body || {});
+    //   })
+    //   .catch((err) => {
+    //     toast.error(err?.message);
+    //     setData({});
+    //   })
+    //   .finally(() => setLoading(false));
   };
 
   return (
     <>
       <PageTitle>{MENU.BN.ORANIZATION_TYPE_TREE}</PageTitle>
-      <div className="row">
-        <div className="col-xl-4 col-md-6 col-12">
-          <Autocomplete
-            placeholder="প্রতিষ্ঠান বাছাই করুন"
-            options={organizationList || []}
-            getOptionLabel={(op) => `${op.nameBn} (${op.nameEn})`}
-            getOptionValue={(op) => op?.id}
-            value={organiztionaData}
-            onChange={(e) => onOrganizationChange(e)}
-          />
-        </div>
-      </div>
       {loading && <ContentPreloader />}
       {!loading && Object.keys(data)?.length > 0 && (
         <OrganizationTemplateTree treeData={data || {}} />
       )}
       {!loading && !(Object.keys(data)?.length > 0) && (
-        <NoData details="কোনো প্রতিষ্ঠানের ট্রি তথ্য পাওয়া যায় নি!" />
+        <NoData details="কোনো প্রতিষ্ঠানের ধরণের ট্রি তথ্য পাওয়া যায় নি!" />
       )}
     </>
   );
