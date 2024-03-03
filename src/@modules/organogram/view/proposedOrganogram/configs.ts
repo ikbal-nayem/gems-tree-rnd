8477388a2bb9df1bf9bc8cabbce14886f3b4133d
Md. Wasifur Rahman <wasif.gems@mopa.gov.bd>
2024-03-03 15:42:30 +0600

    Proposal View

---
+++ src/@modules/organogram/view/proposedOrganogram/configs.ts
@@ -0,0 +1,34 @@
+export const tabs = [
+    {
+      label: "অর্গানোগ্রাম",
+      key: "ORGANOGRAM",
+    },
+    {
+      label: "জনবল",
+      key: "MANPOWER",
+    },
+    {
+      label: "টাস্ক বিল্ডার",
+      key: "TASK_BUILDER",
+    },
+    {
+      label: "জনবলের সারসংক্ষেপ",
+      key: "SUMMARY_OF_MANPOWER",
+    },
+    {
+      label: "যানবাহন, অফিস সরঞ্জাম ও বিবিধ",
+      key: "INVENTORY",
+    },
+    {
+      label: "শব্দসংক্ষেপ",
+      key: "ABBREVIATION",
+    },
+    {
+      label: "সংযুক্ত দপ্তরসমহূ",
+      key: "ATTACHED_ORGANIZATION",
+    },
+    {
+      label: "চেক লিস্ট",
+      key: "CHECKLIST",
+    },
+  ];
--- src/@modules/organogram/view/proposedOrganogram/index.tsx
+++ src/@modules/organogram/view/proposedOrganogram/index.tsx
@@ -6,41 +6,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { TaskBuilder } from "./taskBuilder";
 import ManpowerRequest from "./manpowerRequest";
-
-const tabs = [
-  {
-    label: "অর্গানোগ্রাম",
-    key: "ORGANOGRAM",
-  },
-  {
-    label: "জনবল",
-    key: "MANPOWER",
-  },
-  {
-    label: "টাস্ক বিল্ডার",
-    key: "TASK_BUILDER",
-  },
-  {
-    label: "জনবলের সারসংক্ষেপ",
-    key: "SUMMARY_OF_MANPOWER",
-  },
-  {
-    label: "যানবাহন, অফিস সরঞ্জাম ও বিবিধ",
-    key: "INVENTORY",
-  },
-  {
-    label: "শব্দসংক্ষেপ",
-    key: "ABBREVIATION",
-  },
-  {
-    label: "সংযুক্ত দপ্তরসমহূ",
-    key: "ATTACHED_ORGANIZATION",
-  },
-  {
-    label: "চেক লিস্ট",
-    key: "CHECKLIST",
-  },
-];
+import { tabs } from "./configs";
 
 const ProposedOrganogramView = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
