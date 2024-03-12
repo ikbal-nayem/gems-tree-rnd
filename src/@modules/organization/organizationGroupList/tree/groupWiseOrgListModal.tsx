81ac02ee0061fc0f13463ccb12a7cf9290a05977
pronab.swe <pronab.swe@gmail.com>
2024-03-12 11:53:10 +0600

    org group tree by organization filter done

--- src/@modules/organization/organizationGroupList/tree/groupWiseOrgListModal.tsx
+++ src/@modules/organization/organizationGroupList/tree/groupWiseOrgListModal.tsx
@@ -1,5 +1,5 @@
-// import { ROLES_PERMISSION_TYPES } from "@constants/common.constant";
 import { Icon, Modal } from "@gems/components";
+import { COMMON_LABELS, numEnToBn } from "@gems/utils";
 import { ModalBody } from "react-bootstrap";
 
 interface IGroupWiseOrgListModal {
@@ -14,8 +14,28 @@ const GroupWiseOrgListModal = ({
   onClose,
 }: IGroupWiseOrgListModal) => {
   return (
-    <Modal title={``} isOpen={isOpen} handleClose={onClose}>
-      <ModalBody></ModalBody>
+    <Modal
+      title={`${data?.nameBn} গ্রুপের প্রতিষ্ঠান তালিকা (${numEnToBn(
+        data?.count
+      )} টি)`}
+      isOpen={isOpen}
+      handleClose={onClose}
+      size="lg"
+    >
+      <ModalBody>
+        {data && data?.orgList && data?.orgList?.length > 0 && (
+          <div className="row">
+            {data?.orgList?.map((item, i) => (
+              <div className="col-xl-4 col-md-6 px-1" key={i}>
+                <Icon icon={"check"} className={`me-1`} size={16} />
+                <span className="pb-5">
+                  {item?.nameBn || COMMON_LABELS.NOT_ASSIGN}
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
+      </ModalBody>
     </Modal>
   );
 };
--- src/@modules/organization/organizationGroupList/tree/index.tsx
+++ src/@modules/organization/organizationGroupList/tree/index.tsx
@@ -1,7 +1,14 @@
 import { MENU } from "@constants/menu-titles.constant";
 import { PageTitle } from "@context/PageData";
-import { ContentPreloader, Icon, NoData } from "@gems/components";
+import {
+  Autocomplete,
+  ContentPreloader,
+  Icon,
+  NoData,
+  toast,
+} from "@gems/components";
 import { IObject } from "@gems/utils";
+import { OMSService } from "@services/api/OMS.service";
 import { useEffect, useRef, useState } from "react";
 import GroupWiseOrgListModal from "./groupWiseOrgListModal";
 
@@ -9,23 +16,30 @@ const Tree = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<IObject>({});
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [organiztionaData, setOrganizationData] = useState<IObject>(null);
+  const [organizationList, setOrganizationList] = useState<IObject[]>([]);
   const orgItemData = useRef();
 
   useEffect(() => {
-    getTreeData();
+    OMSService.FETCH.ministryDivisionDepartmentList()
+      .then((resp) => setOrganizationList(resp?.body || []))
+      .catch((err) => toast.error(err?.message));
   }, []);
 
-  const getTreeData = () => {
-    // setLoading(true);
-    // OMSService.FETCH.oranizationTreeByOrganizationId(e?.id)
-    //   .then((res) => {
-    //     setData(res?.body || {});
-    //   })
-    //   .catch((err) => {
-    //     toast.error(err?.message);
-    //     setData({});
-    //   })
-    //   .finally(() => setLoading(false));
+  const onOrganizationChange = (e) => {
+    setOrganizationData(e);
+    if (e?.id) {
+      setLoading(true);
+      OMSService.FETCH.orgGroupTreeByOrganizationId(e?.id)
+        .then((res) => {
+          setData(res?.body || {});
+        })
+        .catch((err) => {
+          toast.error(err?.message);
+          setData({});
+        })
+        .finally(() => setLoading(false));
+    } else setData({});
   };
 
   const onClose = () => {
@@ -36,6 +50,18 @@ const Tree = () => {
   return (
     <>
       <PageTitle>{MENU.BN.ORANIZATION_GROUP_TREE}</PageTitle>
+      <div className="row">
+        <div className="col-xl-4 col-md-6 col-12">
+          <Autocomplete
+            placeholder="প্রতিষ্ঠান বাছাই করুন"
+            options={organizationList || []}
+            getOptionLabel={(op) => `${op.nameBn} (${op.nameEn})`}
+            getOptionValue={(op) => op?.id}
+            value={organiztionaData}
+            onChange={(e) => onOrganizationChange(e)}
+          />
+        </div>
+      </div>
       {loading && <ContentPreloader />}
       {!loading &&
         data?.length > 0 &&
@@ -45,7 +71,7 @@ const Tree = () => {
               <div className="d-flex mb-2">
                 <Icon
                   icon={"add_circle_outline"}
-                  className={`${item?.count > 0 ? "text-muted" : ""} me-2`}
+                  className={`${!(item?.count > 0) ? "text-muted" : ""} me-2`}
                   size={20}
                   disabled={!(item?.count > 0)}
                   onClick={() => {
@@ -55,7 +81,10 @@ const Tree = () => {
                 />
                 <div>
                   {item?.nameBn}
-                  <span className="mb-0 text-gray-600"> {item?.count}</span>
+                  <span className="mb-0">
+                    {" "}
+                    {`(${item?.count || 0})`}
+                  </span>
                 </div>
               </div>
             </div>
