328ff82e34d71d55f0f6859284de10c799b4acc3
pronab.swe <pronab.swe@gmail.com>
2024-04-21 12:43:11 +0600

    alternative post field added and grade mandatory ok in template component

--- src/@modules/organogram-template/components/templateComponent/Tree/Form.tsx
+++ src/@modules/organogram-template/components/templateComponent/Tree/Form.tsx
@@ -77,6 +77,7 @@ const NodeForm = ({
     handleSubmit,
     reset,
     setValue,
+    watch,
     getValues,
     control,
     formState: { errors },
@@ -248,6 +249,10 @@ const NodeForm = ({
       }
       if (noDuplicate) setValue(`manpowerList.${index}.postId`, opt?.id);
     }
+  };
+
+  const onAlternatePostChange = (index, opt) => {
+    setValue(`manpowerList.${index}.alternativePostId`, opt?.id || null);
   };
 
   const getAsyncPostList = useCallback((searchKey, callback) => {
@@ -510,6 +515,31 @@ const NodeForm = ({
                           ?.message as string
                       }
                     />
+                    {watch(`manpowerList.${index}.isHead`) && (
+                      <Autocomplete
+                        label={index < 1 ? "বিকল্প পদবি" : ""}
+                        placeholder="বিকল্প পদবি বাছাই করুন"
+                        // isRequired
+                        isAsync
+                        // isMulti
+                        control={control}
+                        noMargin
+                        getOptionLabel={(op) =>
+                          isNotEnamCommittee ? op?.nameBn : op?.nameEn
+                        }
+                        getOptionValue={(op) => op?.id}
+                        name={`manpowerList.${index}.alternativePostDTO`}
+                        onChange={(t) => onAlternatePostChange(index, t)}
+                        loadOptions={getAsyncPostList}
+                        isError={
+                          !!errors?.manpowerList?.[index]?.alternativePostDTO
+                        }
+                        errorMessage={
+                          errors?.manpowerList?.[index]?.alternativePostDTO
+                            ?.message as string
+                        }
+                      />
+                    )}
                   </div>
 
                   <div className="col-md-6 col-xl-3 px-1">
@@ -517,6 +547,7 @@ const NodeForm = ({
                       label={index < 1 ? "গ্রেড" : ""}
                       placeholder="বাছাই করুন"
                       control={control}
+                      isRequired
                       options={gradeList || []}
                       getOptionLabel={(op) =>
                         isNotEnamCommittee ? op?.nameBn : op?.nameEn
@@ -603,8 +634,7 @@ const NodeForm = ({
 
                   <div
                     className={
-                      "col-md-6 col-xl-1 px-1 d-flex align-items-center " +
-                      (index < 1 ? "mt-5" : "my-0")
+                      "col-md-6 col-xl-1 px-1 " + (index < 1 ? "mt-8" : "mt-2")
                     }
                   >
                     {isHeadIndex === null || isHeadIndex === index ? (
@@ -616,9 +646,19 @@ const NodeForm = ({
                         registerProperty={{
                           ...register(`manpowerList.${index}.isHead`, {
                             onChange: (e) => {
-                              e.target.checked
-                                ? setIsHeadIndex(index)
-                                : setIsHeadIndex(null);
+                              if (e.target.checked) {
+                                setIsHeadIndex(index);
+                              } else {
+                                setIsHeadIndex(null);
+                                setValue(
+                                  `manpowerList.${index}.alternativePostDTO`,
+                                  null
+                                );
+                                setValue(
+                                  `manpowerList.${index}.alternativePostId`,
+                                  null
+                                );
+                              }
                             },
                           }),
                         }}
