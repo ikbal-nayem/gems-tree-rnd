a687e265de312a0c9743910ceb47c39666ba494d
pronab.swe <pronab.swe@gmail.com>
2024-07-28 11:45:32 +0600

    oms module without authnicate issues solved

--- src/routing/AppRoutes.tsx
+++ src/routing/AppRoutes.tsx
@@ -1,23 +1,30 @@
-import { FC } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ErrorsPage } from "../pages/errors/ErrorsPage";
-import { App } from "../App";
-import { PrivateRoutes } from "./PrivateRoutes";
+import { useAuth } from "@context/Auth";
 import { ENV } from "config/ENV.config";
+import { FC, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { App } from "../App";
+import { ErrorsPage } from "../pages/errors/ErrorsPage";
+import { PrivateRoutes } from "./PrivateRoutes";
 
 const PUBLIC_URL = ENV.public_url;
 
 const AppRoutes: FC = () => {
-	return (
-		<BrowserRouter basename={PUBLIC_URL}>
-			<Routes>
-				<Route element={<App />}>
-					<Route path="/*" element={<PrivateRoutes />} />
-					<Route path="error/*" element={<ErrorsPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  const { isAuthenticated, logout } = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated) logout();
+  }, [isAuthenticated]);
+
+  return (
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Routes>
+        <Route element={<App />}>
+          {isAuthenticated && <Route path="/*" element={<PrivateRoutes />} />}
+          <Route path="error/*" element={<ErrorsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export { AppRoutes };
