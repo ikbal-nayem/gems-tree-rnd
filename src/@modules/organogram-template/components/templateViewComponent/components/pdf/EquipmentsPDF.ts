79e0cbd2bb369639aac4a35696b9970a710e9af5
pronab.swe <pronab.swe@gmail.com>
2024-08-14 15:18:44 +0600

    equipment pdf functionality changes done

--- src/@modules/organogram-template/components/templateViewComponent/components/pdf/EquipmentsPDF.ts
+++ src/@modules/organogram-template/components/templateViewComponent/components/pdf/EquipmentsPDF.ts
@@ -3,18 +3,13 @@ import {
   TDocumentDefinitions,
   isListNull,
   isObjectNull,
-  makeBDLocalTime,
   numEnToBn,
 } from "@gems/utils";
-import { commonPDFFooter } from "utility/utils";
 
 export const equipmentPDFContent = (
   data,
-  orgName,
-  versionDate,
   langEn: boolean
 ): TDocumentDefinitions => {
-  const today = makeBDLocalTime(new Date());
   const LABEL = langEn ? LABELS.EN : LABELS.BN;
 
   let inventoryList = [];
@@ -81,9 +76,6 @@ export const equipmentPDFContent = (
 
   return {
     content: [
-      { text: orgName, style: "header" },
-      { text: versionDate, style: "subHeader" },
-      { text: LABEL.EQUIPMENTS, style: "title" },
       ...inventoryList,
       !isListNull(data?.miscelleanousData)
         ? {
@@ -106,43 +98,5 @@ export const equipmentPDFContent = (
           })
         : null,
     ],
-    footer: (currentPage, pageCount) =>
-      commonPDFFooter(currentPage, pageCount, langEn),
-    styles: {
-      header: {
-        fontSize: 14,
-        bold: true,
-        alignment: "center",
-      },
-      subHeader: {
-        fontSize: 10,
-        bold: true,
-        alignment: "center",
-        marginBottom: 10,
-      },
-      title: {
-        fontSize: 12,
-        bold: true,
-        alignment: "left",
-        marginBottom: 8,
-        decoration: "underline",
-      },
-      tableHeader: {
-        bold: true,
-        fontSize: 10,
-        color: "black",
-        alignment: "center",
-      },
-      textBold: {
-        bold: true,
-        fontSize: 14,
-      },
-      olTitle: {
-        fontSize: 11,
-        bold: true,
-        decoration: "underline",
-      },
-    },
-    // pageMargins: [15, 20, 15, 20],
   };
 };
