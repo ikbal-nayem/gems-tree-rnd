835c78d716cc425d9ee8f75ea2e8e13a77de6ef4
pronab.swe <pronab.swe@gmail.com>
2024-08-18 16:18:02 +0600

    sub organogram feature added done in proposal edit tree form

--- src/@modules/organograms/proposal-organogram/components/proposed-organogram-edit-component/tree/Form.tsx
+++ src/@modules/organograms/proposal-organogram/components/proposed-organogram-edit-component/tree/Form.tsx
@@ -23,7 +23,7 @@ import {
 } from "@gems/utils";
 import { CoreService } from "@services/api/Core.service";
 import { OMSService } from "@services/api/OMS.service";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { enCheck } from "utility/checkValidation";
 
@@ -58,6 +58,19 @@ const postTypeList = [
     titleEn: "Non Permanent",
     key: "nonPermanent",
     titleBn: "অস্থায়ী",
+  },
+];
+
+const orgmOrgOrGroupList = [
+  {
+    titleEn: "Organizations",
+    key: "ORGANIZATIONS",
+    titleBn: "প্রতিষ্ঠানসমূহ",
+  },
+  {
+    titleEn: "Organization Group",
+    key: "ORG_GROUP",
+    titleBn: "প্রতিষ্ঠানের গ্ৰুপ",
   },
 ];
 
@@ -124,20 +137,52 @@ const NodeForm = ({
   };
 
   const [titleList, setTitleList] = useState<IObject[]>([]);
+  const [organizationGroupList, setOrganizationGroupList] = useState<IObject[]>(
+    []
+  );
+  const payload = {
+    meta: {
+      page: 0,
+      limit: 1000,
+      sort: [{ order: "asc", field: "serialNo" }],
+    },
+    body: { searchKey: "", orgCategoryGroupId: null },
+  };
+  const orgPayload = useRef(payload);
+
+  useEffect(() => {
+    getOrgGroupList();
+  }, []);
+
+  const getOrgGroupList = () => {
+    OMSService.FETCH.organizationGroupList().then((resp) =>
+      setOrganizationGroupList(resp?.body)
+    );
+  };
+
+  const getAsyncOranizationList = useCallback((searchKey, callback) => {
+    orgPayload.current.body = {
+      ...orgPayload.current.body,
+      searchKey: searchKey ? searchKey?.trim() : "",
+    };
+    OMSService.getEnamOrganizationList(orgPayload?.current).then((resp) =>
+      callback(resp?.body)
+    );
+  }, []);
 
   const onTitleChange = (val, fieldLang: "en" | "bn") => {
     setIsNodeModified(true);
     if (!notNullOrUndefined(val)) return;
     let suggestedValue;
-    if (fieldLang === "en") {
-      suggestedValue = titleList?.find((obj) => obj?.titleEn === val);
-      if (notNullOrUndefined(suggestedValue))
-        setValue("titleBn", suggestedValue?.titleBn);
-    } else {
-      suggestedValue = titleList?.find((obj) => obj?.titleBn === val);
-      if (notNullOrUndefined(suggestedValue))
-        setValue("titleEn", suggestedValue?.titleEn);
-    }
+    // if (fieldLang === "en") {
+    //   suggestedValue = titleList?.find((obj) => obj?.titleEn === val);
+    //   if (notNullOrUndefined(suggestedValue))
+    //     setValue("titleBn", suggestedValue?.titleBn);
+    // } else {
+    suggestedValue = titleList?.find((obj) => obj?.titleBn === val);
+    if (notNullOrUndefined(suggestedValue))
+      setValue("titleEn", suggestedValue?.titleEn);
+    // }
   };
 
   useEffect(() => {
@@ -157,20 +202,6 @@ const NodeForm = ({
 
             return {
               ...item,
-              postDTO:
-                (postList?.length > 0 &&
-                  postList?.find((d) => d?.id === item?.postId)) ||
-                null,
-              gradeDTO:
-                (gradeList?.length > 0 &&
-                  gradeList?.find((d) => d?.id === item?.gradeId)) ||
-                null,
-              serviceTypeDto:
-                (serviceList?.length > 0 &&
-                  serviceList?.find(
-                    (d) => d?.metaKey === item?.serviceTypeKey
-                  )) ||
-                null,
             };
           }),
         };
@@ -347,8 +378,91 @@ const NodeForm = ({
     >
       <form onSubmit={handleSubmit(onFormSubmit)} noValidate>
         <ModalBody>
-          <div className="row mb-4 d-flex justify-content-center">
-            <div className="col-12 col-md-4 col-lg-3 col-xl-2 border rounded-4 p-2 bg-gray-100">
+          <div className="d-flex mb-4 p-1 justify-content-between flex-wrap border rounded-2 bg-gray-100">
+            <div className="d-flex flex-wrap">
+              <div className="me-4">
+                <Checkbox
+                  noMargin
+                  label={"সাব-অর্গানোগ্রাম"}
+                  // isDisabled={isHeadIndex ? isHeadIndex !== index : false}
+                  registerProperty={{
+                    ...register("isSubOrgm", {
+                      onChange: (e) => setValue("subOrgmOrgOrGroupName", null),
+                    }),
+                  }}
+                />
+                {watch("isSubOrgm") === true && (
+                  <div className="mt-3 min-w-200px">
+                    <Select
+                      // label={index < 1 ? "পদের ধরন" : ""}
+                      options={orgmOrgOrGroupList || []}
+                      noMargin
+                      placeholder={"বাছাই করুন"}
+                      // isRequired
+                      textKey={"titleBn"}
+                      // defaultValue={"permanent"}
+                      valueKey="key"
+                      registerProperty={{
+                        ...register(`subOrgmOrgOrGroupName`, {
+                          onChange: () => {
+                            setValue("subOrgmOrgOrGroup.orgGroup", null);
+                            setValue("subOrgmOrgOrGroup.orgList", null);
+                          },
+                        }),
+                      }}
+                      isError={!!errors?.subOrgmOrgOrGroupName}
+                    />
+                  </div>
+                )}
+              </div>
+              {watch("subOrgmOrgOrGroupName") && (
+                <div className="min-w-300px">
+                  {watch("subOrgmOrgOrGroupName") === "ORG_GROUP" ? (
+                    <Autocomplete
+                      label="প্রতিষ্ঠানের গ্ৰুপ"
+                      placeholder="প্রতিষ্ঠানের গ্ৰুপ বাছাই করুন"
+                      name="subOrgmOrgOrGroup.orgGroup"
+                      options={organizationGroupList}
+                      noMargin
+                      // isRequired={
+                      //   isTemplate ? "প্রতিষ্ঠানের গ্ৰুপ বাছাই করুন" : false
+                      // }
+                      control={control}
+                      // autoFocus
+                      getOptionLabel={(op) => op?.nameBn}
+                      getOptionValue={(op) => op?.id}
+                      // onChange={(org) => onOrgGroupChange(org)}
+                      isError={(!!errors?.subOrgmOrgOrGroup as any)?.orgGroup}
+                      errorMessage={
+                        (errors?.subOrgmOrgOrGroup as any)?.orgGroup
+                          ?.message as string
+                      }
+                    />
+                  ) : (
+                    <Autocomplete
+                      label="প্রতিষ্ঠান"
+                      placeholder="প্রতিষ্ঠান বাছাই করুন"
+                      // isRequired="প্রতিষ্ঠান বাছাই করুন"
+                      isAsync
+                      isMulti
+                      control={control}
+                      noMargin
+                      getOptionLabel={(op) => op.nameBn}
+                      getOptionValue={(op) => op?.id}
+                      name={`subOrgmOrgOrGroup.orgList`}
+                      loadOptions={getAsyncOranizationList}
+                      isError={(!!errors?.subOrgmOrgOrGroup as any)?.orgList}
+                      errorMessage={
+                        (errors?.subOrgmOrgOrGroup as any)?.orgList
+                          ?.message as string
+                      }
+                      closeMenuOnSelect={false}
+                    />
+                  )}
+                </div>
+              )}
+            </div>
+            <div>
               <Input
                 label="প্রদর্শন ক্রম"
                 placeholder="প্রদর্শন ক্রম লিখুন"
@@ -360,17 +474,17 @@ const NodeForm = ({
                 type="number"
                 registerProperty={{
                   ...register("displayOrder", {
-                    required: " ",
+                    required: true,
                   }),
                 }}
                 isError={!!errors?.displayOrder}
               />
             </div>
           </div>
-          <div className="row border rounded p-2 my-1 bg-gray-100">
+          <div className="row border rounded p-2 mx-1 my-1 bg-gray-100">
             <div className="col-md-6 col-12">
               <Input
-                label="বাংলা নাম"
+                label="বাংলা নাম (শাখা/সেল/অধিশাখা/অনুবিভাগ)"
                 placeholder="বাংলা নাম লিখুন"
                 isRequired
                 noMargin
@@ -389,7 +503,7 @@ const NodeForm = ({
             </div>
             <div className={"col-md-6 col-12"}>
               <Input
-                label="ইংরেজি নাম"
+                label="ইংরেজি নাম (শাখা/সেল/অধিশাখা/অনুবিভাগ)"
                 placeholder="নাম ইংরেজিতে লিখুন"
                 noMargin
                 registerProperty={{
