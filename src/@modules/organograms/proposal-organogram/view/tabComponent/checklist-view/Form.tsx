829417a6d29604a4740606620b01f170648d0434
pronab.swe <pronab.swe@gmail.com>
2024-09-15 13:49:17 +0600

    checklist title and pdf header added and design changes done

--- src/@modules/organograms/proposal-organogram/view/tabComponent/checklist-view/Form.tsx
+++ src/@modules/organograms/proposal-organogram/view/tabComponent/checklist-view/Form.tsx
@@ -14,8 +14,16 @@ interface IForm {
   data: IObject[];
   onSubmit: (data) => void;
   isSubmitLoading?: boolean;
+  pdfDownloadId?: string;
+  selectedChangeType?: IObject;
 }
-const Form = ({ data, onSubmit, isSubmitLoading }: IForm) => {
+const Form = ({
+  data,
+  onSubmit,
+  isSubmitLoading,
+  pdfDownloadId,
+  selectedChangeType,
+}: IForm) => {
   const {
     register,
     control,
@@ -49,116 +57,126 @@ const Form = ({ data, onSubmit, isSubmitLoading }: IForm) => {
   return (
     <div className="card border p-3">
       <form onSubmit={handleSubmit(onSubmit)}>
-        {fields?.length > 0 &&
-          fields.map((f: IObject, idx) => {
-            return (
-              <div
-                className={`d-flex align-items-top w-100 mt-1 border rounded px-3 my-1`}
-                key={idx}
-              >
-                <div className={f?.chromicNo === 1 ? "mt-8" : "mt-2"}>
-                  <Label> {numEnToBn(f?.chromicNo) + "।"} </Label>
-                </div>
-                <div className="w-100">
-                  {f?.orgChecklistDtoList?.length > 0 &&
-                    f?.orgChecklistDtoList?.map((d, index) => {
-                      return (
-                        <div
-                          className={clsx("ms-2 p-2 row", {
-                            "border-bottom border-2":
-                              f?.orgChecklistDtoList?.length - 1 !== index,
-                          })}
-                          key={index}
-                        >
-                          <div className="col-xl-4">
-                            {idx === 0 && index === 0 && (
-                              <Label className="mb-0 fw-bold">বিষয়</Label>
-                            )}
+        <div id={pdfDownloadId} className="pdfPadding">
+          <div
+            className="pdfHeader text-center"
+            style={{ overflow: "hidden", height: 0 }}
+          >
+            <p className="fs-3 fw-bold">{`${selectedChangeType?.titleBn} এর চেকলিস্ট`}</p>
+          </div>
+          <p className="mb-1">প্রশাসনিক মন্ত্রণালয়/বিভাগ/দপ্তর/সংস্থাঃ</p>
+          {fields?.length > 0 &&
+            fields.map((f: IObject, idx) => {
+              return (
+                <div
+                  className={`d-flex align-items-top w-100 mt-1 border rounded px-3 my-1`}
+                  key={idx}
+                >
+                  <div className={f?.chromicNo === 1 ? "mt-8" : "mt-2"}>
+                    <Label> {numEnToBn(f?.chromicNo) + "।"} </Label>
+                  </div>
+                  <div className="w-100">
+                    {f?.orgChecklistDtoList?.length > 0 &&
+                      f?.orgChecklistDtoList?.map((d, index) => {
+                        return (
+                          <div
+                            className={clsx("ms-2 p-2 row", {
+                              "border-bottom border-2":
+                                f?.orgChecklistDtoList?.length - 1 !== index,
+                            })}
+                            key={index}
+                          >
+                            <div className="col-xl-4">
+                              {idx === 0 && index === 0 && (
+                                <Label className="mb-0 fw-bold">বিষয়</Label>
+                              )}
 
-                            {d?.titleBn}
-                          </div>
-                          <div className="col-xl-1">
-                            {idx === 0 && index === 0 && (
-                              <Label className="mb-0 fw-bold">হ্যাঁ/না</Label>
+                              {d?.titleBn}
+                            </div>
+                            <div className="col-xl-1">
+                              {idx === 0 && index === 0 && (
+                                <Label className="mb-0 fw-bold">হ্যাঁ/না</Label>
+                              )}
+                              <div className="mt-1 mb-2">
+                                <Checkbox
+                                  noMargin
+                                  registerProperty={{
+                                    ...register(
+                                      `orgmChangeList.${idx}.orgChecklistDtoList.${index}.isSubject`
+                                    ),
+                                  }}
+                                />
+                              </div>
+                            </div>
+                            {watch(
+                              `orgmChangeList.${idx}.orgChecklistDtoList.${index}.isSubject`
+                            ) && (
+                              <div className="col-xl-3">
+                                {idx === 0 && index === 0 && (
+                                  <Label className="mb-0 fw-bold" isRequired>
+                                    সংযুক্তি
+                                  </Label>
+                                )}
+                                <SingleFile
+                                  isRequired="ফাইল আপলোড করুন"
+                                  control={control}
+                                  name={`orgmChangeList.${idx}.orgChecklistDtoList.${index}.attachmentFile`}
+                                  onChange={(e) => onFileChange(e, idx, index)}
+                                  isError={
+                                    !!errors?.orgmChangeList?.[idx]
+                                      ?.orgChecklistDtoList?.[index]
+                                      ?.attachmentFile
+                                  }
+                                  errorMessage={
+                                    errors?.orgmChangeList?.[idx]
+                                      ?.orgChecklistDtoList?.[index]
+                                      ?.attachmentFile?.message as string
+                                  }
+                                  // maxSize={3}
+                                />
+                              </div>
                             )}
-                            <div className="mt-1 mb-2">
-                              <Checkbox
+                            <div
+                              className={
+                                watch(
+                                  `orgmChangeList.${idx}.orgChecklistDtoList.${index}.isSubject`
+                                )
+                                  ? "col-xl-4"
+                                  : "col-xl-7"
+                              }
+                            >
+                              {idx === 0 && index === 0 && (
+                                <Label className="mb-0 fw-bold">মন্তব্য</Label>
+                              )}
+                              <Textarea
+                                placeholder={"মন্তব্য লিখুন"}
+                                rows={3}
                                 noMargin
                                 registerProperty={{
                                   ...register(
-                                    `orgmChangeList.${idx}.orgChecklistDtoList.${index}.isSubject`
+                                    `orgmChangeList.${idx}.orgChecklistDtoList.${index}.comment`
                                   ),
                                 }}
-                              />
-                            </div>
-                          </div>
-                          {watch(
-                            `orgmChangeList.${idx}.orgChecklistDtoList.${index}.isSubject`
-                          ) && (
-                            <div className="col-xl-3">
-                              {idx === 0 && index === 0 && (
-                                <Label className="mb-0 fw-bold" isRequired>
-                                  সংযুক্তি
-                                </Label>
-                              )}
-                              <SingleFile
-                                isRequired="ফাইল আপলোড করুন"
-                                control={control}
-                                name={`orgmChangeList.${idx}.orgChecklistDtoList.${index}.attachmentFile`}
-                                onChange={(e) => onFileChange(e, idx, index)}
                                 isError={
                                   !!errors?.orgmChangeList?.[idx]
-                                    ?.orgChecklistDtoList?.[index]
-                                    ?.attachmentFile
+                                    ?.orgChecklistDtoList?.[index]?.comment
                                 }
                                 errorMessage={
                                   errors?.orgmChangeList?.[idx]
-                                    ?.orgChecklistDtoList?.[index]
-                                    ?.attachmentFile?.message as string
+                                    ?.orgChecklistDtoList?.[index]?.comment
+                                    ?.message as string
                                 }
-                                // maxSize={3}
                               />
                             </div>
-                          )}
-                          <div
-                            className={
-                              watch(
-                                `orgmChangeList.${idx}.orgChecklistDtoList.${index}.isSubject`
-                              )
-                                ? "col-xl-4"
-                                : "col-xl-7"
-                            }
-                          >
-                            {idx === 0 && index === 0 && (
-                              <Label className="mb-0 fw-bold">মন্তব্য</Label>
-                            )}
-                            <Textarea
-                              placeholder={"মন্তব্য লিখুন"}
-                              rows={3}
-                              noMargin
-                              registerProperty={{
-                                ...register(
-                                  `orgmChangeList.${idx}.orgChecklistDtoList.${index}.comment`
-                                ),
-                              }}
-                              isError={
-                                !!errors?.orgmChangeList?.[idx]
-                                  ?.orgChecklistDtoList?.[index]?.comment
-                              }
-                              errorMessage={
-                                errors?.orgmChangeList?.[idx]
-                                  ?.orgChecklistDtoList?.[index]?.comment
-                                  ?.message as string
-                              }
-                            />
                           </div>
-                        </div>
-                      );
-                    })}
+                        );
+                      })}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
+
         <div className="d-flex gap-3 justify-content-center mt-3">
           <Button color="primary" type="submit" isLoading={isSubmitLoading}>
             {Object.keys(data)?.length > 0 ? "হালনাগাদ করুন" : "সংরক্ষণ করুন"}
