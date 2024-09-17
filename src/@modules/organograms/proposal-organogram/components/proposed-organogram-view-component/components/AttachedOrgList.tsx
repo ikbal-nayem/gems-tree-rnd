96cc1164caa4fab933fc4fadc1038554ef6ae3d6
pronab.swe <pronab.swe@gmail.com>
2024-09-17 16:44:10 +0600

    attachedOrganization title changes and equipment issues fixed in proposal view component

--- src/@modules/organograms/proposal-organogram/components/proposed-organogram-view-component/components/AttachedOrgList.tsx
+++ src/@modules/organograms/proposal-organogram/components/proposed-organogram-view-component/components/AttachedOrgList.tsx
@@ -64,7 +64,7 @@ const AttachedOrgList = ({
     <div className="card border p-3">
       <div className="card-head d-flex justify-content-between align-items-center">
         <h4 className={title ? "m-0 text-primary" : "m-0"}>
-          {isTabContent && title ? title : LABEL.MAIN_ACTIVITIES}
+          {isTabContent && title ? title : LABEL.ATTACHED_OFFICE}
         </h4>
       </div>
       <Separator className="mt-1 mb-2" />
